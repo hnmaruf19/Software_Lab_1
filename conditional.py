c6fdf6590b0c6abf31637ba8a4624c525680fc0d
hnmaruf19 <marufmaruf09112002@gmail.com>
2025-09-21 15:15:35 +0600

    Add my first file

---
+++ conditional.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if num1>num2:
+    if num1>num3:
+        print(num1, "is big")
+    else:
+        print(num3, "is big")
+elif num2>num3:
+    if num2>num1:
+        print(num2, "is big")
+    else:
+        print(num1, "is big")
+elif num3>num1:
+    if num3>num2:
+        print(num3, "is big")
+    else:
+        print(num2, "is big")
+
